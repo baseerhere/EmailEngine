01b03a5b64d23fdca35c753989962369fc151b99
baseerhere <baseerhere@hotmail.com>
2014-05-19 12:04:50 +0530

    test2

--- MailEngine/Form1.Designer.cs
+++ MailEngine/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MailEngine
 {
-    partial class Form1
+    partial class frmEmailEngine
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.txtEmailAddresses = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(13, 13);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(459, 20);
+            this.txtSubject.TabIndex = 0;
+            this.txtSubject.Text = "Early-stage start-up looking for a non-technical co-founder.";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(13, 40);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBody.Size = new System.Drawing.Size(459, 156);
+            this.txtBody.TabIndex = 1;
+            // 
+            // txtEmailAddresses
+            // 
+            this.txtEmailAddresses.Location = new System.Drawing.Point(13, 206);
+            this.txtEmailAddresses.Multiline = true;
+            this.txtEmailAddresses.Name = "txtEmailAddresses";
+            this.txtEmailAddresses.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEmailAddresses.Size = new System.Drawing.Size(459, 101);
+            this.txtEmailAddresses.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(342, 313);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(130, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 359);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(459, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // frmEmailEngine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(488, 394);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtEmailAddresses);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "frmEmailEngine";
+            this.Text = "Email Engine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtEmailAddresses;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- MailEngine/Form1.cs
+++ MailEngine/Form1.cs
@@ -4,17 +4,105 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MailEngine
 {
-    public partial class Form1 : Form
+    public partial class frmEmailEngine : Form
     {
-        public Form1()
+        public frmEmailEngine()
         {
             InitializeComponent();
         }
+
+
+        public string FromEmailAddress = "rana@email.arizona.edu";
+        public string EmailHostName = "smtpgate.email.arizona.edu";
+        public string EmailPortNo = "587";
+        public string EmailUserName = "rana";
+        public string EmailPassword = "7UJM2WSX4RFV4RFV";
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+            try
+            {
+                // Get emails
+                List<Recipient> _Recipients = new List<Recipient>();
+
+                string _RecupientText = txtEmailAddresses.Text.Trim();
+                string[] _EachRec = _RecupientText.Split('\n');
+                foreach (var item in _EachRec)
+                {
+                    _Recipients.Add(new Recipient { Name = item.Split('\t')[0].Trim(), EmailAddress = item.Split('\t')[1].Trim() });
+                }
+
+                progressBar1.Maximum = _Recipients.Count();
+                progressBar1.Value = 1;
+                string _HtmlText = "";
+                foreach (var item in _Recipients)
+                {
+                    _HtmlText = txtBody.Text;
+
+                    _HtmlText = _HtmlText.Replace("[%Name%]", item.Name.Replace(",",""));
+                    _HtmlText = _HtmlText.Replace("[%email%]", item.EmailAddress);
+
+                    // Send email
+                    SendEmail(item.EmailAddress, _HtmlText, txtSubject.Text);
+                    progressBar1.Value = progressBar1.Value + 1;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            btnSend.Enabled = true;
+
+
+        }
+
+        public bool SendEmail(string _Email, string _Message, string _Subject)
+        {
+            try
+            {
+                //creating object for MailMessage Class
+                MailMessage _Mail = new MailMessage();
+                //Setting values for _Mail object 
+                _Mail.Subject = _Subject;
+                _Mail.Body = _Message;
+                _Mail.IsBodyHtml = true;
+                _Mail.To.Add(new MailAddress(_Email));
+                _Mail.From = new MailAddress(FromEmailAddress,"Rana Suryadevara");
+                _Mail.Bcc.Add(new MailAddress("baseer.intellidate@gmail.com"));
+
+                //Creating Object for SmtpClient Class
+                SmtpClient _Smtp = new SmtpClient();
+
+                //Setting values for SmtpClient class object
+                _Smtp.EnableSsl = true;
+
+                //Setting gmail credentails to smtp server
+                _Smtp.Host = EmailHostName;
+                _Smtp.Port = Convert.ToInt32(EmailPortNo);
+                _Smtp.Credentials = new System.Net.NetworkCredential(EmailUserName, EmailPassword);
+                
+                //Sending Mail Using Smtp Server
+                _Smtp.Send(_Mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Recipient
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
